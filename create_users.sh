44a992816b17093c2e1c0cb52631162a82265019
Erehboio <145766400+Erehboio@users.noreply.github.com>
2026-05-19 18:43:40 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -9,23 +9,10 @@ fi
 for user in "$@"
 do
     useradd -m "$user"
+
+    mkdir -p /home/"$user"/Documents
+    mkdir -p /home/"$user"/Downloads
+    mkdir -p /home/"$user"/Work
+
 done
 
-    HOME_DIR=$(eval echo "~$user")
-
-    mkdir -p "$HOME_DIR/Documents"
-    mkdir -p "$HOME_DIR/Downloads"
-    mkdir -p "$HOME_DIR/Work"
-
-    chown -R "$user:$user" "$HOME_DIR"
-
-    chmod 700 "$HOME_DIR"
-    chmod 700 "$HOME_DIR/Documents"
-    chmod 700 "$HOME_DIR/Downloads"
-    chmod 700 "$HOME_DIR/Work"
-
-    echo "Välkommen $user" > "$HOME_DIR/welcome.txt"
-    echo "Andra användare i systemet:" >> "$HOME_DIR/welcome.txt"
-
-    cut -d: -f1 /etc/passwd | grep -v "^$user$" | sort >> "$HOME_DIR/welcome.txt"
-done
