548abfa8905c8b27c85f6b959ba04291785220ec
Erehboio <145766400+Erehboio@users.noreply.github.com>
2026-05-19 18:44:23 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -14,5 +14,15 @@ do
     mkdir -p /home/"$user"/Downloads
     mkdir -p /home/"$user"/Work
 
-done
+    chown -R "$user:$user" /home/"$user"
+    chmod 700 /home/"$user"
+    chmod 700 /home/"$user"/Documents
+    chmod 700 /home/"$user"/Downloads
+    chmod 700 /home/"$user"/Work
 
+    echo "Välkommen $user" > /home/"$user"/welcome.txt
+    echo "Andra användare i systemet:" >> /home/"$user"/welcome.txt
+
+    cut -d: -f1 /etc/passwd | grep -v "^$user$" >> /home/"$user"/welcome.txt
+
+done
